5b1e2dbdcd1e956e472432d5c784b889bf90ec96
caonimagfw <51797166+caonimagfw@users.noreply.github.com>
2022-06-11 05:22:13 +0800

    Update one.sh

--- 18.06.8/Mine/2022/one.sh
+++ 18.06.8/Mine/2022/one.sh
@@ -140,7 +140,39 @@ mainRun(){
 
 	installbbrv2
 }
+####检查系统类型####
+checkRelease(){
+	if [[ -f /etc/redhat-release ]]; then
+		release="centos"
+	elif cat /etc/issue | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+	elif cat /proc/version | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /proc/version | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+    fi
+}
 
+#####检查系统版本####
+checkReleaseVersion(){
+	if [[ -s /etc/redhat-release ]]; then
+		version=`grep -oE  "[0-9.]+" /etc/redhat-release | cut -d . -f 1`
+	else
+		version=`grep -oE  "[0-9.]+" /etc/issue | cut -d . -f 1`
+	fi
+	bit=`uname -m`
+	if [[ ${bit} = "x86_64" ]]; then
+		bit="x64"
+	else
+		bit="x32"
+	fi
+}
 ####系统检测组件####
 checkRelease
 checkReleaseVersion
